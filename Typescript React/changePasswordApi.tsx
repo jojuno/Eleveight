ad603481149516b99c9c7c56f3589aaa0f051c56
jojuno <35975923+jojuno@users.noreply.github.com>
2018-09-12 10:11:33 -0700

    Delete changePasswordApi.tsx

--- Typescript React/changePasswordApi.tsx
+++
@@ -1,8 +0,0 @@
-﻿import apiExecute from '../common/apiExecute';
-
-const changePassword = data => apiExecute(`/api/App/AppTokens/changePassword`, "PUT", data)
-
-
-export const ChangePasswordApi = {
-    changePassword
-}
