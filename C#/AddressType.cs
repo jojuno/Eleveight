07a773f22ffd6dd30e336af559b12ac04fe1b2e4
jojuno <35975923+jojuno@users.noreply.github.com>
2018-09-12 09:56:32 -0700

    Delete AddressType.cs

--- C#/AddressType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Eleveight.Models.Domain.Common
-{
-    public class AddressType
-    {
-        public int Id { get; set; }
-        public string TypeName { get; set; }
-        public string TypeDescription { get; set; }
-    }
-}
