5a8045b80f4281014d3697c78743592d49db9d10
jojuno <35975923+jojuno@users.noreply.github.com>
2018-09-12 10:12:17 -0700

    Delete searchBarResultsApi.tsx

--- Typescript React/searchBarResultsApi.tsx
+++
@@ -1,8 +0,0 @@
-﻿import apiExecute from '../common/apiExecute';
-import { basename } from 'path';
-
-const getSearchBarResults = (query, pgNum: number = 1) => apiExecute(`/api/search/${query}/${pgNum}`, "GET", null);
-
-export const SearchBarResultsApi = {
-    getSearchBarResults
-}
